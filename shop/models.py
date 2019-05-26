c9219ec59e3f52c2832528bf7de429fc3dafea81
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-26 01:19:28 +0100

    product_list and product_detail done

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Category(models.Model):
@@ -9,6 +10,9 @@ class Category(models.Model):
     ordering = ('name',)
     verbose_name = 'category'
     verbose_name_plural = 'categories'
+
+  def get_absolute_url(self):
+    return reverse('shop:product_list_by_category', args=[self.slug])
 
   def __str__(self):
     return self.name
@@ -28,6 +32,9 @@ class Product(models.Model):
   class Meta:
     ordering = ('name',)
     index_together = (('id', 'slug'),)
+
+  def get_absolute_url(self):
+    return reverse('shop:product_detail', args=[self.id, self.slug])
 
   def __str__(self):
     return self.name
