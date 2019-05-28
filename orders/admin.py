d413211bb723e3e4e2cbb4e4adb1ea39d061564f
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-28 14:22:38 +0100

    weasyprint: pdf invoice generation

--- orders/admin.py
+++ orders/admin.py
@@ -32,6 +32,12 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = "Export to CSV"
 
 
+def order_pdf(obj):
+  return mark_safe('<a href="{}">PDF</a>'.format(reverse('orders:admin_order_pdf', args=[obj.id])))
+
+order_pdf.short_description = "Invoice"
+
+
 def order_details(obj):
   return mark_safe('<a href="{}">View</a>'.format(reverse('orders:admin_order_detail', args=[obj.id])))
 
@@ -43,7 +49,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-  list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_details]
+  list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_details, order_pdf]
   list_filter = ['paid', 'created', 'updated']
   inlines = [OrderItemInline]
   actions = [export_to_csv]
